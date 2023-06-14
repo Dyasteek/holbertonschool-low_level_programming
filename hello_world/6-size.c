b33461ae6d2dd9d8ed3c3e1e5d59420fc9f78046
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-14 14:22:07 -0700

    Syntax correction

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-
-int
-main(void)
+/**
+ * main - Entry point
+ *
+ * Description: Size of
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
-	printf("Size of a char: %u", sizeof(char));
-	printf(" byte(s)\n");
-	printf("Size of an int: %u", sizeof(int));
-	printf(" byte(s)\n");
-	printf("Size of a long int: %u", sizeof(long int));
-	printf(" byte(s)\n");
-	printf("Size of a long long int: %u", sizeof(long long int));
-	printf(" byte(s)\n");
-	printf("Size of float: %u", sizeof(float));
-	printf(" byte(s)\n");
-	return (0);
+printf("Size of a char: %lu", sizeof(char));
+printf(" byte(s)\n");
+printf("Size of an int: %lu", sizeof(int));
+printf(" byte(s)\n");
+printf("Size of a long int: %lu", sizeof(long int));
+printf(" byte(s)\n");
+printf("Size of a long long int: %lu", sizeof(long long int));
+printf(" byte(s)\n");
+printf("Size of a float: %lu", sizeof(float));
+printf(" byte(s)\n");
+return (0);
 }
