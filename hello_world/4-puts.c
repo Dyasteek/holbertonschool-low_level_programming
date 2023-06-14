3a4049c9722a352b0dd04b4e26e276df0fbbc13e
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-14 10:35:44 -0700

    Hello puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-/**
- * main: entry points
- *
- * Description: Hello puts
- *
- * Return: always 0
- */
 
-int main(void) {
+int
+main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
