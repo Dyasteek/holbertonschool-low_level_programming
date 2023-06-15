663fe232b253d103b365427d69ce1a9d59402f27
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-15 08:15:27 -0700

    Numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Description: Size of
- *
- * Return: Always 0
- */
-int main(void)
+
+int
+main(void)
 {
-	int c;
-	for (c = '0'; c <= '9'; ++c)putchar(c);
-	putchar('\n');
-	return (0);
+int c;
+for (c = '0'; c <= '9'; ++c)putchar(c);
+putchar("\n");
+return (0);
 }
