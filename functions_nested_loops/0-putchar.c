efc1862d03daa561ded69fbac3341073bff0238b
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-15 10:26:34 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+int
+main(void)
+{
+char frase[] = "_putchar";
+int i;
+for (i = 0; i <= 7; i++)
+_putchar(frase[i]);
+_putchar('\n');
+return (0);
+}
