86efe153673abf7ad7f041d36e278ac3a1743f73
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-15 08:22:23 -0700

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Size of
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; --c)
+putchar(c);
+putchar('\n');
+return (0);
+}
