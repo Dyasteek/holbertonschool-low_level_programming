a405eec0740e2f35fe0e5df42b2089ca7f43946b
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-16 16:50:35 -0700

    a+b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Min function
+ * @a: an int
+ * @b: also an int
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
