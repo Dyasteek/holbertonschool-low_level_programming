f2c087a9652f07232eba2a7f8ff2e2e960563cb9
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-19 06:16:59 -0700

    maraca

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,25 +1,19 @@
 #include "main.h"
-/**
- * print_last_digit - Entry point
- * @n: char
- * Description: Size of
- *
- * Return: On success 0
- */
-int print_last_digit(int n)
-{
-	int i;
 
-	if (n > 0 || n == 0)
-	{
-		i = n % 10;
-		_putchar(i + '0');
-	}
-	if (n < 0)
-	{
-		n = n * -1;
-		i = n % 10;
-		_putchar(i + '0');
-	}
-	return (i);
+int 
+print_last_digit(int n)
+{
+int i;
+if (n >= 0)
+{
+i = n % 10;
+_putchar(i + '0');
+}
+else
+{
+n = n * -1;
+i = n % 10;
+_putchar(i + '0');
+}
+return (i);
 }
