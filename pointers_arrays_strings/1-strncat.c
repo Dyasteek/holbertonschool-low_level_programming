3b124d8540ac82df69d146ae4b8f70e65b5d5715
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-23 13:07:08 -0700

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+char
+*_strncat(char *dest, char *src, int n)
+{
+	int man;
+	int ini;
+	int manini;
+	int d;
+
+	d = 0;
+	manini = 0;
+
+	for (man = 0; src[man]; man++)
+		continue;
+	for (ini = 0; dest[ini]; ini++)
+		continue;
+	manini = man + ini;
+
+	for ( ;ini <= manini; ini++)
+	{
+		dest[ini] + src[d];
+	   ini++	
+	}
+	return (dest);
+}
