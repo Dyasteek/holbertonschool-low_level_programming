138677469c5cf5627619c2e2204c2cae7246c6fe
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-23 11:53:41 -0700

    Atoi update

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,80 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * _atoi - convert a string to an integer
- *
- * @s: string
- * Return: integer.
- */
+int numStart(char *s);
+int getLength(int n, char *s);
+int getExponent(int len);
 
-int _atoi(char *s)
+int
+_atoi(char *s)
 {
-  int a, b, c, res, sig, size;
-  for (size = 0; s[size] != '\0'; size++)
-    {
-    }
-  for (a = 0; s[a] < '0' || s[a] > '9'; a++)
-    {
-      if (a == size)
+	int i, first, len, aux, num, exp, negatives;
+	unsigned int number;
+
+	first = numStart(s);
+	len = getLength(first, s);
+	aux = first + len;
+	exp = getExponent(len);
+	number = 0;
+
+	for ( ; first <= aux; first++)
 	{
-	  return (0);
+		num = (s[first] - 48) * exp;
+		number = num + number;
+		exp = exp / 10;
 	}
-    }
-  for (; s[a] >= '0' && s[a] <= '9'; a++)
-    {
-      res = (res * 10) + s[a];
-    }
-  for (b = 0; b < a; b++)
-    {
-      if (s[b] == '-')
+
+	negatives = 0;
+
+	for (i = 0; i < (first - len); i++)
 	{
-	  c++;
+		if (s[i] == 45)
+			negatives++;
 	}
-    }
-  if (c % 2 == 0)
-    {
-      sig = 1;
-    }
-  else
-    {
-      sig = -1;
-    }
-  res = res * sig;
-  return (res);
+
+	if (negatives % 2 != 0)
+		number = -number;
+
+	return (number);
+}
+
+int
+getExponent(int len)
+{
+	int exponent;
+
+	for (exponent = 1; len > 0; len--)
+	{
+		exponent = exponent * 10;
+	}
+
+	return (exponent);
+}
+
+int
+getLength(int n, char *s)
+{
+
+	int l = -1;
+
+	while (s[n] >= 48 && s[n] <= 57)
+	{
+		l++;
+		n++;
+	}
+
+	return (l);
+}
+
+int
+numStart(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 47 && s[i] <= 57)
+			break;
+	}
+
+	return (i);
 }
