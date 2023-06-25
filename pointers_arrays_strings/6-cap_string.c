8835378795791226e57d714019db7882c7845c30
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-25 08:01:45 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+char
+*cap_string(char *s)
+{
+int a, b;
+char sig[13] = {9, 10, 32, 33, 40, 41, 44, 46, 59, 63, 34, 123, 125};
+if (s[0] <= 'z' && s[0] >= 'a')
+{
+s[0] = s[0] - 32;
+}
+for (a = 0; s[a] != '\0'; a++)
+{
+for (b = 0; b <= 12; b++)
+{
+if ((s[a - 1] == sig[b]) && (s[a] <= 'z' && s[a] >= 'a'))
+{
+s[a] = s[a] - 32;
+}
+}
+}
+return (s);
+}
