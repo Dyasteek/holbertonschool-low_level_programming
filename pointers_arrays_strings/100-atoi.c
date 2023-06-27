0b3685e88fd4d54bc9871b9e77cef1ea2167a78b
Andrés del Rio <104658304+Dyasteek@users.noreply.github.com>
2023-06-26 22:42:58 -0300

    Final atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,80 +1,31 @@
 #include <stdio.h>
+#include "main.h"
 
-int numStart(char *s);
-int getLength(int n, char *s);
-int getExponent(int len);
-
-int
-_atoi(char *s)
-{
-	int i, first, len, aux, num, exp, negatives;
-	unsigned int number;
-
-	first = numStart(s);
-	len = getLength(first, s);
-	aux = first + len;
-	exp = getExponent(len);
-	number = 0;
-
-	for ( ; first <= aux; first++)
-	{
-		num = (s[first] - 48) * exp;
-		number = num + number;
-		exp = exp / 10;
-	}
-
-	negatives = 0;
-
-	for (i = 0; i < (first - len); i++)
-	{
-		if (s[i] == 45)
-			negatives++;
-	}
-
-	if (negatives % 2 != 0)
-		number = -number;
-
-	return (number);
-}
-
-int
-getExponent(int len)
-{
-	int exponent;
-
-	for (exponent = 1; len > 0; len--)
-	{
-		exponent = exponent * 10;
-	}
-
-	return (exponent);
-}
-
-int
-getLength(int n, char *s)
-{
-
-	int l = -1;
-
-	while (s[n] >= 48 && s[n] <= 57)
-	{
-		l++;
-		n++;
-	}
-
-	return (l);
-}
-
-int
-numStart(char *s)
+int _atoi(char *s)
 {
 	int i;
+	int h, p;
 
+	h = 0;
+	p = -1;
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 47 && s[i] <= 57)
-			break;
-	}
+		if (s[i] == '-')
+			p *= -1;
 
-	return (i);
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
+			else
+				h = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+	}
+	if (p < 0)
+		h *= -1;
+
+	return (h);
 }
