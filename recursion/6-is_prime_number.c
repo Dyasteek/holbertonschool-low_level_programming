617e600a6fb3f3609e7351ba33b7acf4d4a56978
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-28 12:24:51 -0700

    Inception. Is it possible?

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int
+is_prime_number(int n)
+{
+	int b = 2;
+
+	b = chkprime(n, b);
+	return (b);
+}
+
+int
+chkprime(int n, int b)
+{
+	if ((n % b == 0 && n != b) || n <= 1)
+	{
+		return (0);
+	}
+	else if (n == b)
+	{
+		return (1);
+	}
+	else
+	{
+		return (chkprime(n, b));
+	}
+}
