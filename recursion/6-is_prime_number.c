4a454c2abac1fdf0adfd8935b7055aea37689a67
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-28 12:40:09 -0700

    Do you like it Betty?

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,25 +3,26 @@
 int
 is_prime_number(int n)
 {
-int b = 2;
-b = chkprime(n, b);
-return (b);
+	int b = 2;
+
+	b = chkprime(n, b);
+	return (b);
 }
 
 int
 chkprime(int n, int b)
 {
-if ((n % b == 0 && n != b) || n <= 1)
-{
-return (0);
-}
-else if (n == b)
-{
-return (1);
-}
-else
-{
-b++;
-return (chkprime(n, b));
-}
+	if ((n % b == 0 && n != b) || n <= 1)
+	{
+		return (0);
+	}
+	else if (n == b)
+	{
+		return (1);
+	}
+	else
+	{
+		b++;
+		return (chkprime(n, b));
+	}
 }
