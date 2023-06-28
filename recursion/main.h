73f72a23c3b0a960f4711353013c856145ba978d
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-28 12:37:54 -0700

    Syntax corrections

--- recursion/main.h
+++ recursion/main.h
@@ -1,3 +1,5 @@
+#include <unistd.h>
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -6,3 +8,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int chksqrt (int n, int b);
+int is_prime_number(int n);
+int chkprime(int n, int b);
