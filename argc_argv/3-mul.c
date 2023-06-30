538e60ab52b35107d59c188fa5c6e18c10f18f94
Dyasteek-GTW <lawarsenik@gmail.com>
2023-06-30 09:09:19 -0700

    Neither irony nor sarcasm is argument

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char **argv)
+{
+	int a, b;
+	if (argc != 3)
+	{
+		puts("Error");
+		return (1);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", (a *b));
+	}
+	return (0);
+}
