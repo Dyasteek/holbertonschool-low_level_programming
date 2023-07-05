b08d51f1d7d942697e39f94e01ed876668b153d4
Dyasteek-GTW <lawarsenik@gmail.com>
2023-07-05 11:03:26 -0700

    string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -9,23 +9,32 @@ char
 	int strlen1, i, c;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
 	strlen1 = (unsigned int)_strlen(s1);
 	p = malloc((strlen1 + n + 1) * sizeof(char));
 	if (p == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0, c = 0; i < (strlen1 + n); i++)
 	{
 		if (i < strlen1)
+		{
 			p[i] = s1[i];
+		}
 		else
+		{
 			p[i] = s2[c++];
+		}
 	}
 	p[i] = '\0';
-
 	return (p);
 }
 
