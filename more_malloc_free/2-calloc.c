3fc93b8b661580fe7fd235d7d7cb363b9f78c9b0
Dyasteek-GTW <lawarsenik@gmail.com>
2023-07-07 10:03:40 -0700

    This program reserve a little bit of memory

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void
+*_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
