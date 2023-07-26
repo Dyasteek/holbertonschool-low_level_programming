761c276862cc084f42a477359bcc587ea391021c
Dyasteek-GTW <lawarsenik@gmail.com>
2023-07-26 12:39:41 -0700

    Function that frees a list_t list.

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+void
+free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+
+	free(head);
+}
