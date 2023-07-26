0dac4432aa6f46862fb6109ca7a473f1818fc630
Dyasteek-GTW <lawarsenik@gmail.com>
2023-07-26 12:24:22 -0700

    Add node at the end

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+list_t
+*add_node_end(list_t **head, const char *str)
+{
+	list_t *new_l;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new_l = malloc(sizeof(list_t));
+	if (!new_l)
+		return (NULL);
+
+	new_l->str = strdup(str);
+	new_l->len = len;
+	new_l->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_l;
+		return (new_l);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new_l;
+
+	return (new_l);
+}
