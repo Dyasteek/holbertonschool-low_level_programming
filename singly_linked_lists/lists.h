f2aa934d0947872b65d465d50fb25ba665aac42d
Dyasteek-GTW <lawarsenik@gmail.com>
2023-07-26 11:07:49 -0700

    Function that prints all elements of a \'list_t list\'

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,4 +21,5 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 #endif
