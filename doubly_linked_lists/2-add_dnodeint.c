da41fd922e3c3a3b690e51a4414ab3e06208c791
Dyasteek-GTW <lawarsenik@gmail.com>
2023-07-28 06:46:01 -0700

    Syntax improved

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -4,12 +4,14 @@ dlistint_t
 *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *add_node;
+
 	add_node = malloc(sizeof(dlistint_t));
 
 	if (!add_node || !head)
 	{
 		return (NULL);
 	}
+
 	add_node->n = n;
 	add_node->next = *head;
 	*head = add_node;
