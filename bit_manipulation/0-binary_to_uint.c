6489b391b37dfb80b9eb4b68d21e3a007efcd4f7
Dyasteek-GTW <lawarsenik@gmail.com>
2023-07-28 17:29:16 -0700

    Whitespaces improved

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -7,23 +7,23 @@ binary_to_uint(const char *b)
 	int i;
 
 	if (!b)
-    {
+	{
 		return (0);
-    }
+	}
 	for (i = 0; b[i] != '\0'; i++)
-    {
+	{
 		if (b[i]  != '0' && b[i] != '1')
-        {
+		{
 			return (0);
-        }
-    }
+		}
+	}
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		dec_val *= 2;
 		if (b[i] == '1')
-        {
+		{
 			dec_val += 1;
-        }
+		}
 	}
 	return (dec_val);
 }
