bdb821c0582fdecb91f058059c5e3452793e4ebc
DST-Gateway <lawarsenik@gmail.com>
2023-07-28 21:25:32 -0300

    Function that converts a binary number to an unsigned int

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+unsigned int
+binary_to_uint(const char *b)
+{
+	unsigned int dec_val = 0;
+	int i;
+
+	if (!b)
+    {
+		return (0);
+    }
+	for (i = 0; b[i] != '\0'; i++)
+    {
+		if (b[i]  != '0' && b[i] != '1')
+        {
+			return (0);
+        }
+    }
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		dec_val *= 2;
+		if (b[i] == '1')
+        {
+			dec_val += 1;
+        }
+	}
+	return (dec_val);
+}
