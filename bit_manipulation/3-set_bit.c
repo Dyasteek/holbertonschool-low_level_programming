c9518076636edde6b0858439ecb180d3933a8099
DST-Gateway <lawarsenik@gmail.com>
2023-07-29 23:29:28 -0300

    Make that file karnel friendly

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -3,13 +3,13 @@
 int
 set_bit(unsigned long int *n, unsigned int index)
 {
-    if (index <= (sizeof(unsigned long int) * 8))
-    {
-        *n = (*n | (1 << index));
-        return (1);
-    }
-    else
-    {
-        return (-1);
-    }
+	if (index <= (sizeof(unsigned long int) * 8))
+	{
+		*n = (*n | (1 << index));
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
 }
