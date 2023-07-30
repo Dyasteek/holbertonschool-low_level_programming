b5b0298b8ad4102ee85c129e646231bf7ee990e5
DST-Gateway <lawarsenik@gmail.com>
2023-07-29 23:16:37 -0300

    Improved code

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -3,9 +3,12 @@
 int
 get_bit(unsigned long int n, unsigned int index)
 {
-    if (index > (sizeof(unsigned long int) * 8))
-    {
-        return (-1);
-    }
-    return ((n >> index) & 1);
+	if (index <= (sizeof(unsigned long int) * 8))
+	{
+		return ((n >> index) & 1);
+	}
+	else
+	{
+		return (-1);
+	}
 }
