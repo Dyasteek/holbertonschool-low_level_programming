b31b78a0f70a00456faa1f7019e04e94675dcb43
DST-Gateway <lawarsenik@gmail.com>
2023-08-01 13:14:38 -0300

    Function that reads a text file and prints it to the POSIX standard output

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+ssize_t
+read_textfile(const char *filename, size_t letters)
+{
+	long long int desc, cont;
+	char *buff;
+
+	buff = malloc(letters + 1);
+	if (buff == NULL)
+	{
+		return (1);
+	}
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	desc = open(filename, O_RDONLY);
+	if (desc == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	cont = read(desc, buff, letters);
+	if (cont == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	write(STDOUT_FILENO, buff, cont);
+	close(desc);
+	free(buff);
+	return (cont);
+}
