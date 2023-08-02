f7969eb90715d0a73c294290ff31071fa659a779
DST-Gateway <lawarsenik@gmail.com>
2023-08-02 04:31:58 -0300

    Code styled for betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,28 +3,28 @@
 int
 create_file(const char *filename, char *text_content)
 {
-    int fd, write_f;
+int fd, write_f;
 
-    if (!filename)
-    {
-        return (-1);
-    }
-    fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1) /*If it fails*/
-	{
-		return (-1);
-	}
-	if (!text_content)
-    {
-		return (1);
-    }
-	/*Write file*/
-	write_f = write(fd, text_content, strlen(text_content));
-	close(fd);
-	if (write_f == -1) /*If it fails*/
-	{
-		close(fd); /*Close file*/
-		return (-1);
-	}
-	return (1);
+if (!filename)
+{
+return (-1);
+}
+fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (fd == -1) /*If it fails*/
+{
+return (-1);
+}
+if (!text_content)
+{
+return (1);
+}
+/*Write file*/
+write_f = write(fd, text_content, strlen(text_content));
+close(fd);
+if (write_f == -1) /*If it fails*/
+{
+close(fd); /*Close file*/
+return (-1);
+}
+return (1);
 }
