abf378b088cde210716d3bab7dccf027cc3d4e0d
DST-Gateway <lawarsenik@gmail.com>
2023-08-02 04:24:02 -0300

    Function that create a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+int
+create_file(const char *filename, char *text_content)
+{
+    if (!filename)
+    {
+        return (-1);
+    }
+    fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1) /*If it fails*/
+	{
+		return (-1);
+	}
+	if (!text_content)
+    {
+		return (1);
+    }
+	/*Write file*/
+	write_f = write(fd, text_content, strlen(text_content));
+	close(fd);
+	if (write_f == -1) /*If it fails*/
+	{
+		close(fd); /*Close file*/
+		return (-1);
+	}
+	return (1);
+}
