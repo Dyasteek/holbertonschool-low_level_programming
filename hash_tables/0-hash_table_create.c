92e8e63e53bd1d2105f0e1331eb02053f8e6822a
Dyasteek-GTW <lawarsenik@gmail.com>
2023-08-03 06:20:58 -0700

    Code reformulated

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+hash_table_t
+*hash_table_create(unsigned long int size)
+{
+	hash_table_t *new = NULL;
+	hash_node_t **array = NULL;
+
+	new = malloc(sizeof(hash_table_t));
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (new == NULL || array == NULL)
+	{
+		free(new), free(array);
+		return (NULL);
+	}
+	new->size = size;
+	new->array = array;
+	return (new);
+}
