e90bdbe9ebdaaba127974cdc4aeba0fde9462208
Dyasteek-GTW <lawarsenik@gmail.com>
2023-08-03 09:23:15 -0700

    Function that adds an element to the hash table

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+int
+hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new, *temp;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
+	{
+		return (0);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		return (0);
+	}
+	new->value = strdup(value);
+	new->key = strdup(key);
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new;
+	}
+	else
+	{
+		new->next = ht->array[index];
+		ht->array[index] = new;
+	}
+	return (1);
+}
