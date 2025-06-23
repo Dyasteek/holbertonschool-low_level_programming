24e59a6437e2ad3b39925d6c6343167d9ae052ad
Dyasteek <lawarsenik@gmail.com>
2025-06-23 12:28:11 +0000

    Printing the alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Size of
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+putchar(c);
+putchar('\n');
+return (0);
+}
