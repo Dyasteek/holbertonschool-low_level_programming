aa496a2963b9fba3ebd87601dff60632f9c3f430
Dyasteek <lawarsenik@gmail.com>
2025-06-23 12:40:57 +0000

    Alphabet in lowercase followed by a new line

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c;
+    for (c = 'a'; c <= 'z'; ++c)
+        {
+            if (c == 'q' || c == 'e')
+                c = c;
+            else
+                putchar(c);
+        }
+        putchar('\n');
+        return (0);
+}
