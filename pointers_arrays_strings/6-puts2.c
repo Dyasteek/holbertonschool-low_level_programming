906074ecbbf0f9d5e68be61c5cf7476e860e53f4
Dyasteek <lawarsenik@gmail.com>
2025-06-30 16:38:11 +0000

    Stop betty please

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -4,13 +4,13 @@
 void
 puts2(char *str)
 {
-    int a;
-    for (a = 0; *(str + a) != '\0'; a++)
-    {
-        if (a % 2 == 0)
-        {
-            _putchar(str[a]);
-        }
-    }
-    _putchar('\n');
+int a;
+for (a = 0; *(str + a) != '\0'; a++)
+{
+if (a % 2 == 0)
+{
+_putchar(str[a]);
+}
+}
+_putchar('\n');
 }
