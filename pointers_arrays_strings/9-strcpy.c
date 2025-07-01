cd22b0a15aa89c0f0b8632216e63429c1e933078
Dyasteek <lawarsenik@gmail.com>
2025-07-01 04:35:20 +0000

    Deleted strlen func, improved flow

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
-/**
- * _strcpy - copy from read only src to dest
- *
- * Return: the pointer to dest
- */
-/**
- * @dest: where to copy
- */
-/**
- * @src: where to get it from
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-	int len = strlen(src);
-	char tmp;
 
-	for (i = 0; i < len; i++)
+char
+*_strcpy(char *dest, char *src)
+{
+	char *aux1 = dest;
+	for (; *src != '\0'; src++)
 	{
-		tmp = src[i];
-		dest[i] = tmp + '\0';
+		*dest = *src;
+		dest++;
 	}
-	return (dest);
+	*dest = '\0';
+	return (aux1);
 }
