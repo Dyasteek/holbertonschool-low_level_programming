33a91c7c0dda7c3379264cfbd0148d8637deb6e4
Dyasteek <lawarsenik@gmail.com>
2025-07-01 04:36:03 +0000

    Fi betty

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -5,12 +5,12 @@
 char
 *_strcpy(char *dest, char *src)
 {
-	char *aux1 = dest;
-	for (; *src != '\0'; src++)
-	{
-		*dest = *src;
-		dest++;
-	}
-	*dest = '\0';
-	return (aux1);
+char *aux1 = dest;
+for (; *src != '\0'; src++)
+{
+*dest = *src;
+dest++;
+}
+*dest = '\0';
+return (aux1);
 }
