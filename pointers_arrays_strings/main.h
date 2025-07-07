d3fcef1ae4b1a0ed6e95a6653858c22d5420bf0c
Dyasteek <lawarsenik@gmail.com>
2025-07-07 15:56:11 +0000

    Function that locates a character in a string

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,3 +14,4 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
