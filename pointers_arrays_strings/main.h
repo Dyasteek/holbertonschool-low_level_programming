7f1a2573daa370e80d9a74e23147f4b248e96b02
Dyasteek <lawarsenik@gmail.com>
2025-07-07 15:13:59 +0000

    Function that fills memory with a constant byte

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,3 +12,4 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_memset(char *s, char b, unsigned int n);
