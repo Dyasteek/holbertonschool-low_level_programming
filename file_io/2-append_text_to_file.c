f373aaa04da68203372f1fad2bf42a3d9f69a6a3
Dyasteek <lawarsenik@gmail.com>
2025-07-28 13:05:31 -0300

    Funcion access extracted

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,25 +12,21 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t len, appender, permission;
+	ssize_t len, appender;
 
 	if (filename == NULL)
 		return (-1);
-	permission = access(filename, R_OK | W_OK);
-	if (permission == -1)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND, 0644);
-	if (fd != -1)
+	if (text_content != NULL)
 	{
-		if (text_content != NULL)
+		len = strlen(text_content);
+		appender = write(fd, text_content, len);
+		if (appender != len)
 		{
-			len = strlen(text_content);
-			appender = write(fd, text_content, len);
-			if (appender != len)
-			{
-				close(fd);
-				return (-1);
-			}
+			close(fd);
+			return (-1);
 		}
 	}
 	close(fd);
